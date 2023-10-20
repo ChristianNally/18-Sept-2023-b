c27f10a857e47c99f5137b74ee4954dc0dda4140
Andy Lindsay <andydlindsay@gmail.com>
2023-10-20 13:45:07 -0700

    breakout over

--- breakout-express-router/router/server.js
+++ breakout-express-router/router/server.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 
+// require our routers
+const productRouter = require('./routes/product-router');
+const blogpostRouter = require('./routes/blogpost-router');
+
+// use our routers
+app.use('/products', productRouter);
+app.use('/api/admin/blogposts', blogpostRouter); // /api/admin/blogposts
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
